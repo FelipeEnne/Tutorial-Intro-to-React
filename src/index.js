ac5565fe0a0ae6fe646d04ec07ae3cb0998ec28b
FelipeEnne <felipeenne@gmail.com>
2020-04-24 13:11:11 -0300

    function components

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import './index.css'
 
 const root = document.getElementById('root');
 
-class Square extends React.Component {
-
-    render() {
-        return (
-            <button 
-                className="square" 
-                onClick={()=> this.props.onClick()}
-            >
-                {this.props.value}
-            </button>
-        )
-    }
+function Square(props) {
+    return (
+        <button 
+               className="square" 
+               onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
+    
 }
 
 
